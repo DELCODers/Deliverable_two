99c7378cb4514a82fa970145e15011ea1e39b1e0
delmar <delmarpre@gmail.com>
2021-10-29 11:20:50 -0400

    first commit

---
+++ Deliverable 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Deliverable
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            bool shutdown = false;
+            bool foundresponse = false;
+            string inputvalue;
+            string outputvalue = "";
+
+            Console.WriteLine("What would you like to say to the Bot?");
+
+
+
+
+
+            while (!shutdown)
+            {
+                foundresponse = false;
+
+                Console.Write("");
+                inputvalue = Console.ReadLine().ToLower();
+
+                Console.WriteLine("Bot: ");
+
+
+                switch (inputvalue)
+                {
+                    case "hello":
+                        outputvalue = "What else would you like to say to the Bot?";
+                        Console.WriteLine("Hi good to see you");
+                        foundresponse = true;
+                        break;
+
+
+                    case "bye":
+                        outputvalue = "Good bye";
+                        shutdown = true;
+                        break;
+
+                    case "sup":
+                        outputvalue = "What else would you like to say to the Bot?";
+                        Console.WriteLine("I am good");
+                        foundresponse = true;
+                        break;
+
+
+                    case "hello there":
+                        outputvalue = "What else would you like to say to the Bot?";
+                        Console.WriteLine("General kenobi");
+                        foundresponse = true;
+                        break;
+                }
+                if (foundresponse)
+                {
+                    Console.WriteLine(outputvalue);
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+                if (shutdown)
+
+                {
+                    Console.WriteLine("Good bye");
+                }
+
+
+
+
+            }
+        }
+    }
+}
